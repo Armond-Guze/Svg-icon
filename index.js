90dfc5b46cd8a18444369b566f29b4cf34243045
Armond-Guze <Armond.Guze@yahoo.com>
2023-12-25 22:58:59 -0500

    added svg file, added index.js file structure

---
+++ index.js
@@ -0,0 +1,40 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const path = require('path');
+const shapes = require('./lib/shapes');
+
+const questions = [
+    {
+        type: 'input',
+        name: 'text',
+        message: 'Enter up to 3 characters to start your svg.logo.' ,
+        validate: (input) => input.length <= 3
+    }, {
+        type: 'input',
+        name: 'text-color',
+        message: 'Enter a color for your text in your logo.'
+    }, {
+        type: 'checkbox',
+        name: 'shape-name',
+        message: 'Choose a shape for your logo.',
+        choices: ['circle', 'triangle', 'square']
+    }, {
+        type: 'input',
+        name: 'shape-color',
+        message: 'What color do you want your shape logo to be.'
+    }, 
+]
+
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName + '.md'), data);
+}
+
+function init() {
+    inquirer.prompt(questions).then((responses) => {
+        console.log('Generating logo..');
+        console.log(responses)
+        writeToFile('svg.logo',shapes(responses))
+    })
+};
+
+init()
